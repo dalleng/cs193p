85925857b9d6c4faeb206703265692e95c350030
Diego Allen <diegoallen@gmail.com>
2015-03-02 11:22:22 -0300

    Add drawing of axes, double-tap to set origin, panning through graph view and pinch to zoom

--- Calculator/Calculator/GraphView.swift
+++ Calculator/Calculator/GraphView.swift
@@ -8,6 +8,57 @@
 
 import UIKit
 
+@IBDesignable
 class GraphView: UIView {
     
+    var axesDrawer = AxesDrawer()
+    
+    private var origin: CGPoint? {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    private var scale: CGFloat = 50.0 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    override func drawRect(rect: CGRect) {
+        origin = origin ?? self.convertPoint(self.center, fromView: self.superview)
+        axesDrawer.contentScaleFactor = self.contentScaleFactor
+        axesDrawer.drawAxesInRect(self.bounds, origin: origin!, pointsPerUnit: scale)
+    }
+    
+    func changeOrigin(gesture: UITapGestureRecognizer) {
+        if gesture.state == .Ended {
+            origin = gesture.locationInView(self)
+        }
+    }
+    
+    func moveGraph(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Changed, .Ended:
+            origin! += gesture.translationInView(self)
+            gesture.setTranslation(CGPointZero, inView: self)
+        default: break
+        }
+    }
+    
+    func zoomGraph(gesture: UIPinchGestureRecognizer) {
+        if gesture.state == .Changed {
+            scale *= gesture.scale
+            gesture.scale = 1.0
+        }
+    }
+
+}
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (inout left: CGPoint, right: CGPoint) {
+    left = left + right
 }
--- Calculator/Calculator/GraphViewController.swift
+++ Calculator/Calculator/GraphViewController.swift
@@ -9,5 +9,18 @@
 import UIKit
 
 class GraphViewController: UIViewController {
-    
+    @IBOutlet weak var graphView: GraphView! {
+        didSet {
+            // double-tap to change the origin
+            let tgr = UITapGestureRecognizer(target: graphView, action: "changeOrigin:")
+            tgr.numberOfTapsRequired = 2
+            graphView.addGestureRecognizer(tgr)
+            
+            // pan the view to move the graph
+            graphView.addGestureRecognizer(UIPanGestureRecognizer(target: graphView, action: "moveGraph:"))
+            
+            // pinch to zoom
+            graphView.addGestureRecognizer(UIPinchGestureRecognizer(target: graphView, action: "zoomGraph:"))
+        }
+    }
 }
